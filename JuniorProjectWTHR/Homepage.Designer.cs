a40d74d12742a50ef8f057c0c0a4f636e1f9d98a
Marketpricing <rafa.dasw@mail.ugm.ac.id>
2022-11-22 05:15:46 +0700

    Paling terakhir

--- JuniorProjectWTHR/Homepage.Designer.cs
+++ JuniorProjectWTHR/Homepage.Designer.cs
@@ -238,12 +238,15 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(47, 318);
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(33, 299);
             this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.Size = new System.Drawing.Size(41, 15);
             this.label6.TabIndex = 17;
             this.label6.Text = "label6";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // Homepage
             // 
